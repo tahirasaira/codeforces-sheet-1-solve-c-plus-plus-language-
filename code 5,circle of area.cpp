702fec520604804af533b9c8efff48581bdb06e5
Maryam Tahira <132833393+tahirasaira@users.noreply.github.com>
2024-11-05 06:45:33 +0600

    Add files via upload

---
+++ code 5,circle of area.cpp
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    double r;
+    cin>>r;
+    double pi=3.141592653;
+    double area=(pi*r*r);
+
+    printf("%.9lf",area);
+
+    return 0;
+}
